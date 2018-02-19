3862446b76c7ce81c96b48dd5f7223ead365ae6f
Luca <cavalli1234@hotmail.it>
2018-02-19 12:25:21 +0100

    index robustness against unlabeled frame submits

--- source/php_called_scripts/register_labels.py
+++ source/php_called_scripts/register_labels.py
@@ -7,17 +7,8 @@ from php_called_scripts.utils import *
 if __name__ == '__main__':
     labels = sys.argv[1]
     frame = sys.argv[2]
-    tokens = labels.split(',')
-    if len(tokens) != 63:
-        exit(-1)
-    raw_labels = []
-    for idx in range(21):
-        raw_labels.append((float(tokens[3*idx]),
-                           float(tokens[3*idx+1]),
-                           1 if tokens[3*idx+2] in ('true', 'True', 'TRUE') else 0))
-    save_labels(labels=raw_labels, frame=frame)
     if len(sys.argv) > 2:
-        add_contributor(sys.argv[3].replace(" ", ""))
+        contributor = sys.argv[3].replace(" ", "")
     else:
-        add_contributor("Anonymous")
-
+        contributor = None
+    register_labels(labelstring=labels, frame=frame, contributor=contributor)
