c5d7ea81afc6c8e6e0d529675bbce4b58e223124
gianpy15 <gianpaolo.dipietro@mail.polimi.it>
2018-02-20 18:13:51 +0100

    Update web front-end

--- web/javascript/hand_tracking_samples.js
+++ web/javascript/hand_tracking_samples.js
@@ -1,28 +1,37 @@
-try {
-   window.onload = () => loadImages();
-} catch (error){
-   alert("error");
-}
+var junctions;
+var l_canvas = document.getElementById("left_c_points");
+var r_canvas = document.getElementById("right_c_points");
+var undo_b = document.getElementById("undo_b");
+var submit_b = document.getElementById("submit_b");
+var new_sample_b = document.getElementById("new_sample_b");
 
+l_canvas.addEventListener('click', e => on_click_events(e, 'l'), false);
+l_canvas.addEventListener('contextmenu', e => on_click_events(e, 'r'), false);
+undo_b.addEventListener('click', () => junctions.undo(), false);
+submit_b.addEventListener('click', e => on_commit_event(e, true), false);
+new_sample_b.addEventListener('click', () => on_commit_event(false), false);
+
+window.onload = function () {
+    loadImages();
+    junctions = new Points(l_canvas, r_canvas, target_joints);
+};
 
 function loadImages() {
-   try{
-	   var l_canvas = document.getElementById("left_c");
-      var r_canvas = document.getElementById("right_c");
-	   var l_ctx = l_canvas.getContext("2d");
-      var r_ctx = r_canvas.getContext("2d");
+    try{
+        var l_canvas = document.getElementById("left_c");
+        var r_canvas = document.getElementById("right_c");
+        var l_ctx = l_canvas.getContext("2d");
+        var r_ctx = r_canvas.getContext("2d");
 
-	   var left_img_path = "http://clipartbarn.com/wp-content/uploads/2016/10/Hand-clip-art-free-clipart-images.jpg";
-	   var other_url = 'https://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png'
-	   var left_img = new Image();
-      var right_img = new Image();
+        var left_img = new Image();
+        var right_img = new Image();
 
-	   right_img.onload = () => r_ctx.drawImage(right_img, 0, 0, r_canvas.width, r_canvas.height);
-      left_img.onload = () => l_ctx.drawImage(left_img, 0, 0, l_canvas.width, l_canvas.height);
+        right_img.onload = () => r_ctx.drawImage(right_img, 0, 0, r_canvas.width, r_canvas.height);
+        left_img.onload = () => l_ctx.drawImage(left_img, 0, 0, l_canvas.width, l_canvas.height);
 
-	   left_img.src = other_url;
-      right_img.src = left_img_path;
-    }catch(error){
+        left_img.src = target_img_url;
+        right_img.src = sample_img_url;
+    } catch(error) {
 		alert(error)
 	}
 }
