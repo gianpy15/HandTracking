07bf1dd0799a50fb5cbcb4a339bb54bf8f8ffd78
Mattia Di Fatta <mattia.difatta@mail.polimi.it>
2018-02-27 16:47:17 +0100

    added choice of the video form

--- source/gui/video_player.py
+++ source/gui/video_player.py
@@ -19,6 +19,7 @@ from gui.player_thread import PlayerThread
 from gui.model_drawer import *
 from hand_data_management.video_loader import load_labeled_video
 import threading
+from tkinter.simpledialog import askstring
 
 if __name__ == '__main__':
     # global variables
@@ -33,76 +34,81 @@ if __name__ == '__main__':
     cmd_width = 90
     cmd_height = image_height
 
-    # Load frames and labels
-    vidname = "snap"
-    frames, labels, indexes = load_labeled_video(vidname, gapflags=True)  # TODO add choice of video
-
     # Root window of GUI
     root = Tk()
     root.title("Player")
 
-    topframe = Frame(root, height=image_height, width=cmd_width+image_width)
-    topframe.pack(side=TOP)
+    # Choose video name
+    vidname = askstring(title="Type video's name", parent=root, prompt="Video name:")
 
-    # Frame for buttons
-    cmd = Frame(topframe, height=cmd_height, width=cmd_width)
-    cmd.pack(side=RIGHT)
+    if vidname != None and vidname != "":
 
-    # Canvas for frames and labels
-    canvas = Canvas(topframe, width=image_width, height=image_height)
-    canvas.pack(side=LEFT)
+        # Load frames and labels
+        # vidname = "snap"
+        frames, labels, indexes = load_labeled_video(vidname, gapflags=True)
 
-    # Play button
-    play_button = Button(cmd, text="Play")
-    play_button.pack(fill=BOTH)
+        topframe = Frame(root, height=image_height, width=cmd_width+image_width)
+        topframe.pack(side=TOP)
 
-    # Pause button
-    pause_button = Button(cmd, text="Pause")
-    pause_button.pack(fill=BOTH)
+        # Frame for buttons
+        cmd = Frame(topframe, height=cmd_height, width=cmd_width)
+        cmd.pack(side=RIGHT)
 
-    # Labeled/unlabeled message field
-    msg = StringVar()
-    frame_status = Label(root, textvariable=msg)
-    frame_status.pack()
+        # Canvas for frames and labels
+        canvas = Canvas(topframe, width=image_width, height=image_height)
+        canvas.pack(side=LEFT)
 
-    # Discard commands
-    discard_button = Button(cmd, text="Discard")
-    discard_button.pack(fill=BOTH)
-    discard = StringVar()
-    discard_label = Label(root, textvariable=discard)
-    discard_label.pack()
+        # Play button
+        play_button = Button(cmd, text="Play")
+        play_button.pack(fill=BOTH)
 
-    # Save button
-    save_button = Button(cmd, text="Save")
-    save_button.pack(fill=BOTH)
+        # Pause button
+        pause_button = Button(cmd, text="Pause")
+        pause_button.pack(fill=BOTH)
 
-    # Build the model drawer
-    md = ModelDrawer()
-    md.set_target_area(canvas)
+        # Labeled/unlabeled message field
+        msg = StringVar()
+        frame_status = Label(root, textvariable=msg)
+        frame_status.pack()
 
-    # Build and run the thread
-    player = PlayerThread(
-        frames=frames,
-        labels=labels,
-        canvas=canvas,
-        modeldrawer=md,
-        status=msg,
-        indexes=indexes,
-        discard=discard
-    )
-    pause_button.bind('<Button-1>', lambda e: player.pause())
-    play_button.bind('<Button-1>', lambda e: player.play())
-    root.after(50, player.nextframe, root)
+        # Discard commands
+        discard_button = Button(cmd, text="Discard")
+        discard_button.pack(fill=BOTH)
+        discard = StringVar()
+        discard_label = Label(root, textvariable=discard)
+        discard_label.pack()
 
-    slider = Scale(root,
-                   from_=-2., to=3., resolution=0.05,
-                   orient=HORIZONTAL, tickinterval=1.0,
-                   command=lambda v: player.set_speed_mult(v))
-    slider.set(1)
-    slider.pack(side=BOTTOM, fill=BOTH)
+        # Save button
+        save_button = Button(cmd, text="Save")
+        save_button.pack(fill=BOTH)
 
-    discard_button.bind('<Button-1>', lambda e: player.set_changes())
+        # Build the model drawer
+        md = ModelDrawer()
+        md.set_target_area(canvas)
 
-    save_button.bind('<Button-1>', lambda e: player.print_changes("snap"))
+        # Build and run the thread
+        player = PlayerThread(
+            frames=frames,
+            labels=labels,
+            canvas=canvas,
+            modeldrawer=md,
+            status=msg,
+            indexes=indexes,
+            discard=discard
+        )
+        pause_button.bind('<Button-1>', lambda e: player.pause())
+        play_button.bind('<Button-1>', lambda e: player.play())
+        root.after(50, player.nextframe, root)
 
-    root.mainloop()
+        slider = Scale(root,
+                       from_=-2., to=3., resolution=0.05,
+                       orient=HORIZONTAL, tickinterval=1.0,
+                       command=lambda v: player.set_speed_mult(v))
+        slider.set(1)
+        slider.pack(side=BOTTOM, fill=BOTH)
+
+        discard_button.bind('<Button-1>', lambda e: player.set_changes())
+
+        save_button.bind('<Button-1>', lambda e: player.print_changes("snap"))
+
+        root.mainloop()
