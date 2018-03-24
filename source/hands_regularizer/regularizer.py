cd255f82677f9219752b219f48d42aab3d231f4a
MatteoBiasielli <matteo.biasielli@mail.polimi.it>
2018-03-24 13:52:55 +0100

    ultimated regularizer

--- source/hands_regularizer/regularizer.py
+++ source/hands_regularizer/regularizer.py
@@ -3,15 +3,16 @@ from scipy.misc import imresize
 import scipy
 import hands_bounding_utils.utils as u
 
-PARAMS = 'PARAMS' #
+PARAMS = 'PARAMS'
 NORMALIZE_AVG_VARIANCE = 'NORM_A_V'
-RGB2GREY = 'RGB2GREY' #
-RESIZEPERC = 'RESIZEPERC' #
-RESIZEFIX = 'RESIZEFIX' #
-PADDING = 'PADDING' #
-OPS = 'OPS' #
+RGB2GREY = 'RGB2GREY'
+RESIZEPERC = 'RESIZEPERC'
+RESIZEFIX = 'RESIZEFIX'
+PADDING = 'PADDING'
+OPS = 'OPS'
 
-class RegularizerParametersCreator:
+
+class Regularizer:
     def __init__(self):
         self.pars = dict()
         self.pars[OPS] = []
@@ -31,29 +32,59 @@ class RegularizerParametersCreator:
         self.pars[OPS].append(RESIZEFIX)
         self.pars[RESIZEFIX] = [height, width]
 
+    def normalize(self):
+        self.pars[OPS].append(NORMALIZE_AVG_VARIANCE)
 
-def __add_padding(image, right_pad, bottom_pad):
+    def apply(self, frame):
+        frame = np.array(frame)
+        for op in self.pars[OPS]:
+            if op == PADDING:
+                params = self.pars[PADDING]
+                frame = add_padding(frame, params[0], params[1])
+                continue
+            if op == RGB2GREY:
+                frame = rgb2gray(frame)
+                continue
+            if op == RESIZEPERC:
+                frame = imresizeperc(frame, self.pars[RESIZEPERC])
+                continue
+            if op == RESIZEFIX:
+                frame = fixed_resize(frame, self.pars[RESIZEFIX])
+                continue
+            frame = normalize(frame)
+        return frame
+
+
+def add_padding(image, right_pad, bottom_pad):
     image = np.hstack((image, np.zeros([image.shape[0], right_pad, image.shape[2]], dtype=image.dtype)))
     image = np.vstack((image, np.zeros([bottom_pad, image.shape[1], image.shape[2]], dtype=image.dtype)))
     return image
 
 
-def __rgb2gray(rgb):
+def rgb2gray(rgb):
     r, g, b = rgb[:, :, 0], rgb[:, :, 1], rgb[:, :, 2]
     gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
     return gray.reshape([gray.shape[0], gray.shape[1], 1])
 
 
-def __imresizeperc(frame, rate):
+def imresizeperc(frame, rate):
     return imresize(frame, rate)
 
 
-def __fixed_resize(frame, size):
+def fixed_resize(frame, size):
     return imresize(frame, size)
 
 
-test = scipy.misc.imread("t.jpg")
-test = np.array(test)
+def normalize(frame):
+    avg = frame.mean()
+    std = frame.std()
+    frame = (frame - avg)/std
+    return frame
 
-res = __fixed_resize(test, (250, 200))
+
+test = scipy.misc.imread("t.jpg")
+r = Regularizer()
+r.padding(50, 50)
+r.fixresize(50, 50)
+res = r.apply(test)
 u.showimage(res)
