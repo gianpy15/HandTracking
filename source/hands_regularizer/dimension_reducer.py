705cc66624d5902ca074470c9771aea628de49dd
MatteoBiasielli <matteo.biasielli@mail.polimi.it>
2018-03-26 21:17:29 +0200

    minor changes

--- source/hands_regularizer/dimension_reducer.py
+++ source/hands_regularizer/dimension_reducer.py
@@ -2,6 +2,7 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from hands_bounding_utils.hands_locator_from_rgbd import *
 import hands_bounding_utils.utils as u
+import hands_regularizer.regularizer as reg
 
 
 dataset_path = pm.resources_path(os.path.join("hands_bounding_dataset", "network_test"))
@@ -31,3 +32,26 @@ print(len(pca.components_))
 for i in range(4):
     u.showimage(images[i])
     u.showimage(approx_images[i])
+
+
+
+##### CROPS #####
+crops = []
+num = len(images)
+for i in range(num):
+    i_th_crop = u.get_crops_from_heatmap(images[i], heat_maps[i], height_shrink_rate=4, width_shrink_rate=4,
+                                         enlarge=0.8, accept_crop_minimum_dimension_pixels=600)
+    crops.append(i_th_crop)
+
+# crops should now contain all crops. crops[i] contains all the crops
+# of the i-th image in a list (in case there are more hands)
+
+r = reg.Regularizer()
+# size of crops
+r.fixresize(100, 100)
+
+crops_final = []
+for lista in crops:
+    crops_final.append(r.apply_on_batch(lista))
+ # crops_final should contain all crops of the same size
+
