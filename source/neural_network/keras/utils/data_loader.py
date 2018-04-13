86efae2b2acf83236a63e251e9391e9860439ef2
Luca <cavalli1234@hotmail.it>
2018-04-13 20:04:34 +0200

    refactored data loader

--- source/neural_network/keras/utils/data_loader.py
+++ source/neural_network/keras/utils/data_loader.py
@@ -18,6 +18,19 @@ def load_dataset(train_samples, valid_samples, data_format=CROPPER,
                  separate_valid=True):
     merge_vids = False
 
+    if data_format == CROPPER:
+        read_dataset_random = croputils.read_dataset_random
+        read_dataset = croputils.read_dataset
+        create_dataset = create_crop_dataset
+        need_expand_greys = True
+        double_target = False
+    else:
+        read_dataset_random = jlocutils.read_dataset_random
+        read_dataset = jlocutils.read_dataset
+        create_dataset = create_joint_dataset
+        need_expand_greys = False
+        double_target = True
+
     if dataset_path is None:
         dataset_path = crops_path() if data_format == CROPPER else joints_path()
 
@@ -29,27 +42,16 @@ def load_dataset(train_samples, valid_samples, data_format=CROPPER,
             videos_list = build_videos
         if verbose:
             print("Building dataset on videos %s" % videos_list)
-        if data_format == CROPPER:
-            croputils.create_dataset_shaded_heatmaps(savepath=dataset_path, fillgaps=True,
-                                                     resize_rate=0.5, width_shrink_rate=4, heigth_shrink_rate=4,
-                                                     videos_list=videos_list)
-        else:
-            img_reg = regularizer.Regularizer()
-            img_reg.fixresize(200, 200)
-            hm_reg = regularizer.Regularizer()
-            hm_reg.fixresize(100, 100)
-            hm_reg.heatmaps_threshold(.5)
-            jlocutils.create_dataset(savepath=dataset_path, fillgaps=True, im_regularizer=img_reg,
-                                     heat_regularizer=hm_reg, enlarge=.5, cross_radius=5,
-                                     videos_list=videos_list)
+        create_dataset(videos_list=videos_list,
+                       dataset_path=dataset_path)
+
 
     dataset_info = _get_available_dataset_stats(dataset_path)
-    print(dataset_info)
+
     train_vids, valid_vids = choose_train_valid_videos(dataset_info, train_samples, valid_samples)
     traincount = available_frames(dataset_info, train_vids)
     validcount = available_frames(dataset_info, valid_vids)
-    print(train_vids)
-    print(valid_vids)
+
     if validcount < valid_samples or traincount < train_samples:
         availability = traincount + validcount
         requested = train_samples + valid_samples
@@ -72,17 +74,6 @@ def load_dataset(train_samples, valid_samples, data_format=CROPPER,
     if verbose:
         print("Chosen train videos: %s" % train_vids)
         print("Chosen valid videos: %s" % valid_vids)
-
-    if data_format == CROPPER:
-        read_dataset_random = croputils.read_dataset_random
-        read_dataset = croputils.read_dataset
-        need_expand_greys = True
-        double_target = False
-    else:
-        read_dataset_random = jlocutils.read_dataset_random
-        read_dataset = jlocutils.read_dataset
-        need_expand_greys = False
-        double_target = True
 
     if random_dataset:
         if merge_vids:
@@ -233,3 +224,59 @@ def available_frames(dataset_info, vidlist):
     for vid in vidlist:
         count += dataset_info[vid]
     return count
+
+
+def create_crop_dataset(videos_list, dataset_path):
+    croputils.create_dataset_shaded_heatmaps(savepath=dataset_path, fillgaps=True,
+                                             resize_rate=0.5, width_shrink_rate=4, heigth_shrink_rate=4,
+                                             videos_list=videos_list)
+
+
+def create_joint_dataset(videos_list, dataset_path):
+    img_reg = regularizer.Regularizer()
+    img_reg.fixresize(200, 200)
+    hm_reg = regularizer.Regularizer()
+    hm_reg.fixresize(100, 100)
+    hm_reg.heatmaps_threshold(.5)
+    jlocutils.create_dataset(savepath=dataset_path, fillgaps=True, im_regularizer=img_reg,
+                             heat_regularizer=hm_reg, enlarge=.5, cross_radius=5,
+                             videos_list=videos_list)
+
+
+def load_joint_dataset(train_samples, valid_samples,
+                       random_dataset=False,
+                       shuffle=True,
+                       build_videos=None,
+                       dataset_path=None,
+                       verbose=False,
+                       separate_valid=True):
+    return load_dataset(train_samples=train_samples,
+                        valid_samples=valid_samples,
+                        data_format=JLOCATOR,
+                        random_dataset=random_dataset,
+                        shuffle=shuffle,
+                        use_depth=False,
+                        build_videos=build_videos,
+                        dataset_path=dataset_path,
+                        verbose=verbose,
+                        separate_valid=separate_valid)
+
+
+def load_crop_dataset(train_samples, valid_samples,
+                      random_dataset=False,
+                      shuffle=True,
+                      use_depth=False,
+                      build_videos=None,
+                      dataset_path=None,
+                      verbose=False,
+                      separate_valid=True):
+    return load_dataset(train_samples=train_samples,
+                        valid_samples=valid_samples,
+                        data_format=CROPPER,
+                        random_dataset=random_dataset,
+                        shuffle=shuffle,
+                        use_depth=use_depth,
+                        build_videos=build_videos,
+                        dataset_path=dataset_path,
+                        verbose=verbose,
+                        separate_valid=separate_valid)
