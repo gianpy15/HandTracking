deeccdad5a6b59d10d2a6eff19c23a77e3109ad8
gianpaolo <gianpaolo.dipietro@mail.polimi.it>
2018-04-13 16:56:33 +0200

    Add script for video completion

---
+++ source/completion.py
@@ -0,0 +1,22 @@
+import sys
+import os
+sys.path.append(os.path.realpath(os.path.join(os.path.split(__file__)[0], "..")))
+
+from hand_data_management.utils import *
+
+if __name__ == '__main__':
+    videos = os.listdir(framebase)
+    videos = [nm for nm in videos if os.path.isdir(os.path.join(framebase, nm))]
+    compls = {}
+    for vid in videos:
+        idxcontent = get_index_content(vid)
+        count = 0
+        for i in range(len(idxcontent)):
+            if idxcontent[i] == FLAG_LABELED:
+                count += 1
+        completion = count / len(idxcontent)
+        compls[vid] = completion
+
+    compls_sorted = sorted(compls, key=compls.__getitem__, reverse=True)
+    for vid in compls_sorted:
+        print("Video completion for {}: {:.2f}%".format(vid, compls[vid]*100))
