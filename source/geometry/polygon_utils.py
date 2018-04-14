d0575b43b9eb819f289d5555bb31e40ebc071185
Luca <cavalli1234@hotmail.it>
2018-04-14 13:04:26 +0200

    jit compiled polygon test

--- source/geometry/polygon_utils.py
+++ source/geometry/polygon_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as mplt
+from numba import jit
 from timeit import timeit
 
 
@@ -16,6 +17,46 @@ def compute_angle(p1, p2, p3):
     p3n = p3-p2
     anglesin = (p1n[0]*p3n[1]-p1n[1]*p3n[0])/(np.linalg.norm(p1n)*np.linalg.norm(p3n))
     return np.arcsin(anglesin)
+
+
+
+# ##################### ARCSIN TABLE #######################
+
+@jit
+def make_arcsin_table(resolution):
+    x = np.arange(-1.0, 1.0, 2 / resolution)
+    y = np.arcsin(x)
+    return np.array(y)
+
+@jit
+def table_asin(value, table):
+    return table[int((value + 1.) * len(table)/2.)]
+
+# #################### FASTER VERSION ####################
+
+
+def fast_is_inside(point, polygon):
+    return contains_origin(polygon - point)
+
+@jit
+def contains_origin(polygon):
+    # normalizing once for all times
+    norm_poly = np.empty(shape=(len(polygon), 2), dtype=np.float32)
+    for idx in range(len(polygon)):
+        norm_poly[idx] = polygon[idx] / np.linalg.norm(polygon[idx])
+
+    # computing angles WRT origin
+    angle = 0.
+    for idx in range(len(norm_poly)-1):
+        angle += fast_compute_angle(norm_poly[idx], norm_poly[idx+1])
+    angle += fast_compute_angle(norm_poly[len(norm_poly)-1], norm_poly[0])
+    return angle > np.pi or angle < -np.pi
+
+@jit
+def fast_compute_angle(p1, p2):
+    return np.arcsin(p1[0]*p2[1] - p1[1]*p2[0])
+
+# #################### TEST UTILS ##########################
 
 
 def make_circle(resol, center, radius, startdelta=0.):
@@ -37,23 +78,40 @@ def rotate(point, angle):
 if __name__ == '__main__':
     low = -5
     high = 5
-    npoints = 1000
+    npoints = 10000
     plist = np.random.uniform(low=low, high=high, size=(npoints, 2))
     poly = make_circle(resol=700,
                        center=[2, 0],
                        radius=2,
                        startdelta=0.3)
+    if True:
+        def speedtest():
+            for p in plist:
+                fast_is_inside(p, poly)
 
-    def speedtest():
+        print(timeit(speedtest, number=1))
         for p in plist:
-            is_inside(p, poly)
+            intest = fast_is_inside(p, poly)
+            col = "green" if intest else "red"
+            mplt.plot(p[0], p[1], marker="o", color=col)
 
-    print(timeit(speedtest, number=1))
+        mplt.fill(poly[:, 0], poly[:, 1], fill=False)
+        mplt.show()
+    else:
+        resol = 5000
+        table = make_arcsin_table(resol)
+        x = np.arange(-1, 1, 0.001)
 
-    for p in plist:
-        intest = is_inside(p, poly)
-        col = "green" if intest else "red"
-        mplt.plot(p[0], p[1], marker="o", color=col)
+        def asin_speed_test():
+            for s in x:
+                np.arcsin(s)
 
-    mplt.fill(poly[:, 0], poly[:, 1], fill=False)
-    mplt.show()
+        def table_speed_test():
+            for s in x:
+                table_asin(s, table)
+
+        print(timeit(asin_speed_test, number=1))
+        print(timeit(table_speed_test, number=1))
+        mplt.plot(x, np.arcsin(x), color='green')
+        mplt.plot(x, [table_asin(s, table) for s in x], color='blue')
+        mplt.show()
