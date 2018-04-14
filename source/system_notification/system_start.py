185485673131ccee37480787aa77187e2a07f24a
gianpaolo <gianpaolo.dipietro@mail.polimi.it>
2018-04-14 22:52:29 +0200

    Add script for notify system start

--- source/system_notification/system_start.py
+++ source/system_notification/system_start.py
@@ -7,11 +7,10 @@ import datetime
 import socket
 import utils.telegram_bot as tele
 
-
 if __name__ == "__main__":
     string = "###### System started ######\n"
     string += "\t\t" + socket.gethostname() + "\n"
     actual_time = datetime.datetime.now()
     string += "\t\tat time: {}/{}/{} {}:{}:{}\n".format(actual_time.day, actual_time.month, actual_time.year,
-                                                           actual_time.hour, actual_time.minute, actual_time.second)
-    tele.send_message(string)
+                                                        actual_time.hour, actual_time.minute, actual_time.second)
+    tele.send_message(string, disable_notification=True)
