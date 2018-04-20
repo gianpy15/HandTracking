c54055aaabd44b10341727d7a72cf59a3dcc369f
gianpaolo <gianpaolo.dipietro@mail.polimi.it>
2018-04-20 15:36:31 +0200

    Add training script for u_net

---
+++ source/runnables/networks/to_train/heatmap_u_net.py
@@ -0,0 +1,67 @@
+import os
+import sys
+
+sys.path.append(os.path.realpath(os.path.join(os.path.split(__file__)[0], "..", "..", "..")))
+
+from library.neural_network.keras.custom_layers.heatmap_loss import *
+from library.neural_network.tensorboard_interface.tensorboard_manager import TensorBoardManager as TBManager
+from data.datasets.data_loader import load_dataset
+from data.naming import *
+from library.neural_network.keras.models.u_net import u_net
+from library.neural_network.keras.training.model_trainer import train_model
+import keras.regularizers as kr
+from data.augmentation.data_augmenter import Augmenter
+from data.regularization.regularizer import Regularizer
+import numpy as np
+
+
+if __name__ == '__main__':
+    model = 'cropper_u_net_v1'
+
+    m1_path = cropper_h5_path(model)
+
+    TBManager.set_path("heat_maps")
+    train = True
+
+    # Hyper parameters
+    weight_decay = kr.l2(1e-5)
+    learning_rate = 1e-4
+
+    # Load data
+    dataset = load_dataset(train_samples=4000,
+                           valid_samples=1000,
+                           use_depth=False)
+
+    # Augment data
+    log("Augmenting data...")
+    augmenter = Augmenter()
+    augmenter.shift_hue(prob=0.25).shift_sat(prob=0.25).shift_val(prob=0.25)
+    dataset[TRAIN_IN] = augmenter.apply_on_batch(dataset[TRAIN_IN])
+    dataset[VALID_IN] = augmenter.apply_on_batch(dataset[VALID_IN])
+    log("Augmentation end")
+
+    # Regularize data
+    log("Regularizing data...")
+    regularizer = Regularizer()
+    regularizer.normalize()
+    dataset[TRAIN_IN] = regularizer.apply_on_batch(dataset[TRAIN_IN])
+    dataset[VALID_IN] = regularizer.apply_on_batch(dataset[VALID_IN])
+    log("Regularization end")
+
+    input_shape = np.shape(dataset[TRAIN_IN][0])
+    print("Input shape: {}".format(input_shape))
+
+    # Build up the model
+    # Model with high penalty for P(x = 1 | not hand)
+    model1 = train_model(dataset=dataset,
+                         model_generator=lambda: u_net(input_shape=input_shape, weight_decay=weight_decay,
+                                                       dropout_rate=0.5),
+                         loss=lambda x, y: prop_heatmap_penalized_fn_loss(x, y, -1.5, 2),
+                         learning_rate=learning_rate,
+                         patience=5,
+                         tb_path="heat_maps/" + model,
+                         model_name=model,
+                         model_type=CROPPER,
+                         batch_size=5,
+                         epochs=100,
+                         verbose=True)
