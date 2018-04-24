c1e91c585841a2bdff850374f5e79cb7af58fcc4
Luca <cavalli1234@hotmail.it>
2018-04-25 01:26:33 +0200

    asynchronous dataset reader impl

--- source/data/datasets/reading/formatting.py
+++ source/data/datasets/reading/formatting.py
@@ -14,3 +14,22 @@ JUNCHEATFORMAT = ('heatmap_array', __heatmap_dequantize)
 JUNCVISFORMAT = ('visible', lambda x: x)
 
 
+IN = 'in'
+TARGET = 'target'
+SEC_TARGET = 'sec_target'
+
+CROPS_STD_FORMAT = {
+    IN: (CROPIMGFORMAT,),
+    TARGET: (CROPHEATMAPFORMAT,)
+}
+
+CROPS_STD_DEPTH_FORMAT = {
+    IN: (CROPIMGFORMAT, CROPDEPTHFORMAT),
+    TARGET: (CROPHEATMAPFORMAT,)
+}
+
+JUNC_STD_FORMAT = {
+    IN: (JUNCIMGFORMAT,),
+    TARGET: (JUNCHEATFORMAT,),
+    SEC_TARGET: (JUNCVISFORMAT,)
+}
