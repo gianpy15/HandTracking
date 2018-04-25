4a853fdc8bd99f8227be07c9473073ce18f538f9
Luca <cavalli1234@hotmail.it>
2018-04-25 21:23:44 +0200

    dataset async reader implicit priority on requested batches

--- source/data/datasets/reading/dataset_manager.py
+++ source/data/datasets/reading/dataset_manager.py
@@ -16,116 +16,151 @@ class DatasetManager:
         ThreadPoolManager.get_thread_pool().submit(fn=self.__separate_and_load)
         self.train_batch_number = None
         self.valid_batch_number = None
-        self.train_availability = 0
-        self.valid_availability = 0
         self.current_train_batch_index = 0
         self.current_valid_batch_index = 0
-        self.traindata = None
-        self.validdata = None
+        self.batchdata = None
         self.main_lock = threading.Condition()
+        self.loading_done = False
+        self.trainframes = None
+        self.validframes = None
+        self.urgent_queue = []
 
     def __separate_and_load(self):
         separator = DatasetSeparator(self.dataset_dir)
-        trainframes, validframes = separator.select_train_validation_framelists(self.train_samples,
+        self.trainframes, self.validframes = separator.select_train_validation_framelists(self.train_samples,
                                                                                 self.valid_samples)
-        train_avail_tot = len(trainframes)
-        valid_avail_tot = len(validframes)
+        train_avail_tot = len(self.trainframes)
+        valid_avail_tot = len(self.validframes)
 
         self.train_batch_number = int(math.ceil(train_avail_tot / self.batch_size))
         self.valid_batch_number = int(math.ceil(valid_avail_tot / self.batch_size))
-        self.traindata = [None for _ in range(self.train_batch_number)]
-        self.validdata = [None for _ in range(self.valid_batch_number)]
-        end = 0
-        for idx in range(self.train_batch_number):
-            start = end
-            end = (idx + 1) * self.batch_size
-            data = read_formatted_batch(frames=trainframes[start:end],
+        self.batchdata = [None for _ in range(self.train_batch_number + self.valid_batch_number)]
+        while not self.loading_done:
+            idx, frames = self.get_next_batch()
+            if idx is None:
+                break
+            assert self.batchdata[idx] is None
+            log("DATA LOADING WORKER: loading %s batch %d" % ("train" if idx < self.train_batch_number else "valid",
+                                                              idx % self.train_batch_number),
+                level=DEBUG)
+            data = read_formatted_batch(frames=frames,
                                         formatdict=self.formatting)
             self.main_lock.acquire()
-            self.traindata[idx] = data
+            self.batchdata[idx] = data
             self.main_lock.notify_all()
             self.main_lock.release()
+        log("DATA LOADING WORKER: quitting")
 
-        end = 0
-        for idx in range(self.valid_batch_number):
-            start = end
-            end = (idx + 1) * self.batch_size
-            data = read_formatted_batch(frames=validframes[start:end],
-                                        formatdict=self.formatting)
-            self.main_lock.acquire()
-            self.validdata[idx] = data
-            self.main_lock.notify_all()
-            self.main_lock.release()
+    def get_next_batch(self):
+        assert self.batchdata is not None
+        index = None
+        # try to determine next index to process by popping the urgent queue first
+        while index is None and len(self.urgent_queue) > 0:
+            index = self.urgent_queue.pop()
+            if self.batchdata[index] is not None:
+                index = None
+        # if no urgent batch has been requested, pick the first uncompleted one
+        if index is None:
+            for tidx in range(len(self.batchdata)):
+                if self.batchdata[tidx] is None:
+                    index = tidx
+                    break
+        # if no batch is uncompleted, we are done
+        if index is None:
+            self.loading_done = True
+            return None, None
+        # first we have train batches, if index is in the train range:
+        if index < self.train_batch_number:
+            start = self.batch_size * index
+            end = start + self.batch_size
+            return index, self.trainframes[start:end]
+        # else we are in the validation batches section:
+        else:
+            start = self.batch_size * (index - self.train_batch_number)
+            end = start + self.batch_size
+            return index, self.validframes[start:end]
 
     def get_training_batch(self, index=None, blocking=True):
-        index = self.current_train_batch_index if index is None else index
+        index = index or self.current_train_batch_index
         if not blocking:
-            if self.traindata is None or self.traindata[index] is None:
+            if self.batchdata is None:
                 return None
-            return self.traindata[index]
+            index = min(index, self.train_batch_number-1)
+            return self.batchdata[index]
 
         self.main_lock.acquire()
-        self.main_lock.wait_for(predicate=lambda: self.traindata is not None)
+        self.main_lock.wait_for(predicate=lambda: self.batchdata is not None)
         index = min(index, self.train_batch_number-1)
-        self.main_lock.wait_for(predicate=lambda: self.traindata[index] is not None)
+        if self.batchdata[index] is None:
+            self.urgent_queue.append(index)
+        self.main_lock.wait_for(predicate=lambda: self.batchdata[index] is not None)
         self.main_lock.release()
         self.current_train_batch_index = (index + 1) % self.train_batch_number
-        return self.traindata[index]
+        return self.batchdata[index]
 
     def get_training_batch_number(self, blocking=True):
         if not blocking:
             return self.train_batch_number
         self.main_lock.acquire()
-        self.main_lock.wait_for(predicate=lambda: self.traindata is not None)
+        self.main_lock.wait_for(predicate=lambda: self.batchdata is not None)
         self.main_lock.release()
         return self.train_batch_number
 
     def get_validation_batch(self, index=None, blocking=True):
-        index = self.current_valid_batch_index if index is None else index
+        index = index or self.current_valid_batch_index
         if not blocking:
-            if self.validdata is None or self.validdata[index] is None:
+            if self.batchdata is None:
                 return None
-            return self.validdata[index]
+            index = min(index, self.valid_batch_number-1)
+            return self.batchdata[self.train_batch_number + index]
 
         self.main_lock.acquire()
-        self.main_lock.wait_for(predicate=lambda: self.validdata is not None)
+        self.main_lock.wait_for(predicate=lambda: self.batchdata is not None)
         index = min(index, self.valid_batch_number-1)
-        self.main_lock.wait_for(predicate=lambda: self.validdata[index] is not None)
+        realindex = self.train_batch_number + index
+        if self.batchdata[realindex] is None:
+            self.urgent_queue.append(realindex)
+        self.main_lock.wait_for(predicate=lambda: self.batchdata[realindex] is not None)
         self.main_lock.release()
         self.current_valid_batch_index = (index + 1) % self.valid_batch_number
-        return self.validdata[index]
+        return self.batchdata[realindex]
 
     def get_validation_batch_number(self, blocking=True):
         if not blocking:
             return self.valid_batch_number
         self.main_lock.acquire()
-        self.main_lock.wait_for(predicate=lambda: self.validdata is not None)
+        self.main_lock.wait_for(predicate=lambda: self.batchdata is not None)
         self.main_lock.release()
         return self.valid_batch_number
 
 
 if __name__ == '__main__':
     from matplotlib.pyplot import imshow, show
+    import time
     dm = DatasetManager(train_samples=5000,
-                        valid_samples=4200,
-                        batch_size=2,
+                        valid_samples=5000,
+                        batch_size=100,
                         dataset_dir=crops_path(),
                         formatting=CROPS_STD_FORMAT)
     print('starting ds reading...')
     batches = []
-    for idx in range(dm.get_training_batch_number()):
-        print('getting training batch %d' % idx)
-        batches.append(dm.get_training_batch())
-        print('got training batch %d!' % idx)
-
+    set_verbosity(DEBUG)
+    t1 = time.time()
     for idx in range(dm.get_validation_batch_number()):
-        print('getting valid batch %d' % idx)
+        t = time.time()
+        print('REQUEST: valid batch %d' % idx)
         batches.append(dm.get_validation_batch())
-        print('got valid batch %d!' % idx)
+        print('RESULT valid batch %d in %f ms' % (idx, 1000*(time.time()-t)))
 
-    for batch in batches[:3]:
-        for img in batch[IN]:
-            imshow(img)
-            show()
+    for idx in range(dm.get_training_batch_number()):
+        t = time.time()
+        print('REQUEST: train batch %d' % idx)
+        batches.append(dm.get_training_batch())
+        print('RESULT: train batch %d in %f ms' % (idx, 1000*(time.time()-t)))
+    print('total time spent: %f ms' % (1000*(time.time()-t1)))
+    # for batch in batches:
+    #     for img in batch[IN]:
+    #        imshow(img)
+    #        show()
 
 
