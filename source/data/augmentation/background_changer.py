255dfe5be97d0da9d9042855c644d16cd3e95d76
gianpy15 <gianpaolo.dipietro@mail.polimi.it>
2018-05-07 14:41:16 +0200

    small fix

--- source/data/augmentation/background_changer.py
+++ source/data/augmentation/background_changer.py
@@ -1,11 +1,14 @@
 import numpy as np
 
+
 def change_background(hand_img: np.ndarray, background_img: np.ndarray, filter):
     if hand_img.shape != background_img.shape:
         return
     for row in filter:
         for pixel in row:
             if pixel is True:
+                pass
 
 
 def mean_hsv(img: np.ndarray):
+    pass
