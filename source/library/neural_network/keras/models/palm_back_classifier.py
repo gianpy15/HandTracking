489bd7997f50fd962d55b4bc0428d799e88f039c
MatteoBiasielli <matteo.biasielli@mail.polimi.it>
2018-05-18 17:18:17 +0200

    minor

--- source/library/neural_network/keras/models/palm_back_classifier.py
+++ source/library/neural_network/keras/models/palm_back_classifier.py
@@ -12,45 +12,43 @@ def l2_reg(weight_matrix):
 
 
 def simple_classifier_rgb(weight_decay=None):
-    model = kmod.Sequential()
+    inputs = klayers.Input(shape=(200, 200, 3), name=IN(0))
 
-    model.add(klayers.Conv2D(input_shape=(200, 200, 3), filters=50, kernel_size=[3, 3], activation='relu', name=IN(0)))
+    x = klayers.Conv2D(filters=50, kernel_size=[3, 3], activation='relu')(inputs)
 
-    model.add(klayers.Conv2D(filters=60, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=60, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.Conv2D(filters=60, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=60, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.MaxPooling2D(pool_size=[2, 2]))
+    x = klayers.MaxPooling2D(pool_size=[2, 2])(x)
 
-    model.add(klayers.Conv2D(filters=70, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=70, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.Conv2D(filters=80, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=80, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.Conv2D(filters=90, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=90, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.MaxPooling2D(pool_size=[2, 2]))
+    x = klayers.MaxPooling2D(pool_size=[2, 2])(x)
 
-    model.add(klayers.Conv2D(filters=100, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=100, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.Conv2D(filters=150, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=150, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.MaxPooling2D(pool_size=[2, 2]))
+    x = klayers.MaxPooling2D(pool_size=[2, 2])(x)
 
-    model.add(klayers.Conv2D(filters=170, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=170, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.Conv2D(filters=180, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=180, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.MaxPooling2D(pool_size=[2, 2]))
+    x = klayers.MaxPooling2D(pool_size=[2, 2])(x)
 
-    model.add(klayers.Conv2D(filters=200, kernel_size=[3, 3], activation='relu'))
+    x = klayers.Conv2D(filters=200, kernel_size=[3, 3], activation='relu')(x)
 
-    model.add(klayers.Flatten())
+    x = klayers.Flatten()(x)
 
-    model.add(klayers.Dense(units=100, activation='relu', use_bias=True))
+    x = klayers.Dense(units=100, activation='relu', use_bias=True)(x)
 
-    #model.add(klayers.Dropout(rate=0.1, seed=12345))
+    x = klayers.Dense(units=1, activation='sigmoid', name=OUT(0))(x)
 
-    model.add(klayers.Dense(units=1, activation='sigmoid', name=OUT(0)))
-
-    return model
+    return kmod.Model(inputs=(inputs,), outputs=(x,))
 
