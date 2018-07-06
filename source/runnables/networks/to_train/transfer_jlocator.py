9849d558b65450da0d27fadaed454ed1063c1673
Luca <cavalli1234@hotmail.it>
2018-07-06 17:30:57 +0200

    server sync

--- source/runnables/networks/to_train/transfer_jlocator.py
+++ source/runnables/networks/to_train/transfer_jlocator.py
@@ -53,7 +53,7 @@ learning_rate = 1e-5  # >0
 # in the target heatmaps even when they appear in different proportions.
 # This parameter changes a little bit the equilibrium favouring the white (when positive)
 # This may solve the problem of the network outputting either full-black or full-white heatmaps
-white_priority = -.5  # any value, 0 is perfect equilibrium
+white_priority = -.2  # any value, 0 is perfect equilibrium
 
 # how much the heatmap loss is scaled up against the visibility loss.
 # makes sure that the algorithm gives the right priority to losses
@@ -62,7 +62,7 @@ heat_priority = 100
 # the extra importance to give to false positives.
 # Use this parameter to increase the penalty of saying there is a hand where there is not.
 # The penalty is proportional and additive: delta=6 means we will add 6 times the penalty for false positives.
-delta = 8  # >=-1, 0 is not additional penalty, -1<delta<0 values discount penalty. delta<=-1 PROMOTES MISTAKES.
+delta = 1  # >=-1, 0 is not additional penalty, -1<delta<0 values discount penalty. delta<=-1 PROMOTES MISTAKES.
 
 
 # NETWORK PARAMETERS
